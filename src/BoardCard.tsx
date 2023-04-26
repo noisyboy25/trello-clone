9e8ac98e03544026e575e1c1d2a12d3b836c9f6f
noisyboy25 <dj402and@gmail.com>
2023-04-26 04:58:56 +0300

    Add auto-resize textarea

--- src/BoardCard.tsx
+++ src/BoardCard.tsx
@@ -1,12 +1,16 @@
-import { Card, Text, CardBody } from '@chakra-ui/react';
+import { Card, CardBody, Editable, EditablePreview } from '@chakra-ui/react';
 import { CardInfo } from './App';
+import AutoResizeTextarea from './AutoResizeTextarea';
 
 const BoardCard = ({ cardInfo }: { cardInfo: CardInfo }) => {
   return (
     <Card w={'16em'} key={cardInfo.id}>
       {/* <CardHeader>{card.title}</CardHeader> */}
       <CardBody textAlign={'start'}>
-        <Text>{cardInfo.description}</Text>
+        <Editable defaultValue={cardInfo.description}>
+          <EditablePreview />
+          <AutoResizeTextarea />
+        </Editable>
       </CardBody>
     </Card>
   );
